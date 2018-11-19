9354ec45fba9708c37fa7d7da81a778dd0a4981b
jamruzanka <m.w.jamrog@gmail.com>
2018-11-19 21:18:43 +0100

    script used to take data from .txt and put it into .xlxs

---
+++ program_excel_demographic.py
@@ -0,0 +1,21 @@
+from openpyxl import Workbook
+
+excel_movies = Workbook()
+ws1 = excel_movies.active
+ws2 = excel_movies.create_sheet("Sheet_2")
+
+my_file = open('Demograpfic.txt', 'r')
+column_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+row_number = 0
+column_number = 0
+for line in my_file:
+    elements = line.split(",")
+    print(">>>>",elements)
+    for element in elements:
+        key = column_letters[column_number]+str(row_number + 1)
+        print(">>>>",key,element)
+        ws1[key] = element
+        column_number += 1
+    row_number += 1
+    column_number = 0
+excel_movies.save("DemographicData.xlsx")
